4421f36f6c6a3938e73c8fb0efcaf1be432f872f
krivoruky <73437477+krivoruky@users.noreply.github.com>
2022-04-28 23:07:09 +0300

    add background

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import s from './Card.module.sass'
 
 export default function Card({id, title, background}) {
   return (
-    <div className={s.card}>
+    <div className={s.card} style={{backgroundColor: background}}>
         <p>{title}</p>
         <Button/>
     </div>
